2a8f8de0ffc08e619190e75308647286afc248a9
Abdulazeez Jimoh <abdulazeezojimoh@gmail.com>
2022-01-01 16:05:51 +0100

    Update anpd.py

--- src/anpd.py
+++ src/anpd.py
@@ -5,31 +5,33 @@ import time as time
 
 import cv2 as cv2
 import easyocr
-import imutils
 from imutils.video import FileVideoStream, VideoStream
 
 
 class ANPD():
 
     def __init__(self, anpdPath, confThreshold, nmsThreshold):
-        self.__configPath = f"{anpdPath}anpd.cfg"
-        self.__weightsPath = f"{anpdPath}out/anpd_best.weights"
-        self.__namesPath = f"{anpdPath}anpd.names"
+        self.__detectorConfig = f"{anpdPath}/detector/anpd.cfg"
+        self.__detectorWeights = f"{anpdPath}/detector/anpd_best.weights"
+        self.__detectorNames = f"{anpdPath}/detector/anpd.names"
+        self.__readerPath = f"{anpdPath}/reader/"
+
         self.confThreshold = confThreshold
         self.nmsThreshold = nmsThreshold
 
         print("[INFO] loading ANPD...")
         self.__detector = cv2.dnn_DetectionModel(cv2.dnn.readNetFromDarknet(
-            self.__configPath,
-            self.__weightsPath))
+            self.__detectorConfig,
+            self.__detectorWeights))
         self.__detector.setInputParams(
             scale=1/255, size=(416, 416), swapRB=True)
 
-        with open(self.__namesPath, 'r') as f:
-            self.__names = f.read().splitlines()
+        with open(self.__detectorNames, 'r') as f:
+            self.__detectorNames = f.read().splitlines()
 
-        self.__reader = easyocr.Reader(['en'], gpu=False, detector=False,
-                                       verbose=False)
+        self.__reader = easyocr.Reader(['en'], False, self.__readerPath,
+                                       detector=False, verbose=False)
+        self.__readerChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890'
 
     def __detect(self, frame):
         start = time.time()
@@ -43,6 +45,11 @@ class ANPD():
 
         return nameIds, scores, boxes
 
+    def __clean(self, plate):
+        gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+
+        return gray
+
     def __read(self, frame, detection):
         nameIds, scores, boxes = detection
         texts = []
@@ -51,9 +58,15 @@ class ANPD():
 
         for box in boxes:
             x, y, w, h = box
+            plate = self.__clean(frame[y:y+h, x:x+w])
 
-            text = self.__reader.readtext(frame[y:y+h, x:x+w], detail=0)
-            texts.append(text[0].upper())
+            text = self.__reader.recognize(plate, detail=0,
+                                           allowlist=self.__readerChars)
+
+            if len(text) > 0:
+                texts.append(text[0].upper())
+            else:
+                texts.append('')
 
         end = time.time()
         print("[INFO] ANPR took {:.6f} seconds".format(end - start))
@@ -69,7 +82,8 @@ class ANPD():
             bb_color = (0, 255, 255)
             text_color = (0, 0, 0)
 
-            text = '{} | {}[{:.0%}]'.format(text, self.__names[nameId], score)
+            text = '{} | {}[{:.0%}]'.format(
+                text, self.__detectorNames[nameId], score)
             text_bb, _ = cv2.getTextSize(
                 text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
 
